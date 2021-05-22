a8425a7163f93000b055eb42b348c21ac12c008f
ru14 <74753345+ru14@users.noreply.github.com>
2021-05-22 11:31:37 -0700

    Model and some code

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ The user should be able to:
 
   * Add exercises to the most recent workout plan.
 
-  * Add new exercises to a new workout plan.
+  * Add new exercises to a new workout plan
 
   * View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
 
---
+++ server.js
@@ -0,0 +1,129 @@
+/*
+0. install mongodb packages, express, 
+1. fill out server.js with express boilerplate code
+2. express will need to serve the public folder
+3. you will write your routes on the express side, uysing the routes already written in the api.js file
+4. write your schema and connection.js for mongo
+5. write the mongodb code to take the res.json or whatever input you receive and send it to your mongodb database
+
+*/
+
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+
+
+
+const PORT = process.env.PORT || 3000;
+
+const User = require("./userModel.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//const databaseUrl = "notetaker";
+const collections = ["notes"];
+
+const db = mongoose(databaseUrl, collections);
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+
+  db.notes.insert(req.body, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get("/all", (req, res) => {
+  db.notes.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.get("/find/:id", (req, res) => {
+  db.notes.findOne(
+    {
+      _id: mongoose.ObjectId(req.params.id)
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.post("/update/:id", (req, res) => {
+  db.notes.update(
+    {
+      _id: mongoose.ObjectId(req.params.id)
+    },
+    {
+      $set: {
+        title: req.body.title,
+        note: req.body.note,
+        modified: Date.now()
+      }
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  db.notes.remove(
+    {
+      _id: mongoose.ObjectID(req.params.id)
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.delete("/clearall", (req, res) => {
+  db.notes.remove({}, (error, response) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(response);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
