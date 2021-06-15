30f51933d04af28034a5cab4bd55745a38a60ab2
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-14 21:31:05 -0700

    not worked

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,13 +6,13 @@ const path = require("path")
 
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/index.html", "build"));
+    res.sendFile(path.join(__dirname + "./public/index.html"));
   });
   router.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html","build"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
   router.get("/stats", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html", "build"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
 
